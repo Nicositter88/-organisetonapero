28845d168bcd3f34c1e74c18a88c09bf15899b13
Nicolas Sitter-Nolleau <nicolas.sitternolleau@gmail.com>
2017-02-01 22:31:52 +0100

    add stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+
+  get '.well-known/acme-challenge/IC7wP_DoE43zk2JSS-11PSETT8LFiTy_4fUzJyW9d2I', to: 'pages#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
